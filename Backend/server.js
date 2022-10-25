65a164e7984547ebec55fb7e1f3f79fed65a7d04
Manassaharoy <triptoroy5@gmail.com>
2022-10-25 15:08:34 +0600

    up

--- Backend/server.js
+++ Backend/server.js
@@ -91,12 +91,12 @@ mongoose.connect(process.env.DATABASE_CONNECT, function (err, res) {
 app.use(express.json());
 app.use(
   cors({
-    origin: '*',
+    // origin: '*',
     // origin: true,
-    // origin: ["https://mindschoolbd.com/","https://www.mindschoolbd.com/","mindschoolbd.com"],
-    // methods: "GET,POST,PUT,DELETE",
-    // credentials: true,
-    // exposedHeaders: ['x-auth-token']
+    origin: ["https://mindschoolbd.com/","https://www.mindschoolbd.com/","mindschoolbd.com"],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+    exposedHeaders: ['x-auth-token']
   })
 );
 app.use(
